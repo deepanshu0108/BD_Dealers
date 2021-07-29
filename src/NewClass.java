4d94d5e2a15cbd707065d3de07fb9ca1b61554a4
deepanshu0108 <77038428+deepanshu0108@users.noreply.github.com>
2021-07-29 23:43:01 +0530

    Add files via upload

---
+++ src/NewClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author IVPS
+ */
+public class NewClass {
+
+    public static String c1="",c2="",c3="";
+
+
+}
---
+++ src/gv.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author IVPS
+ */
+public class gv {
+public static String z;
+public static String v;
+public static String p;
+public static String q;
+public static String u;
+public static String t;
+public static String a;
+public static String b;
+public static String c;
+public static String d;
+}
